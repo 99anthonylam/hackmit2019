582998c057bc25e461bc1d1e3de1498fe17735c5
Cindy Zhang <cindyz8735@gmail.com>
2019-09-14 15:17:38 -0400

    save typed text to js

---
+++ static/editor.js
@@ -0,0 +1,23 @@
+$( document ).ready(function() {
+
+    var curr_data;
+    var timeoutId;
+    $('form input, form textarea').on('input propertychange change', function() {
+        
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(function() {
+            // Runs 1 second (1000 ms) after the last change    
+            saveToDB();
+        }, 1000);
+    });
+
+    function saveToDB()
+    {
+        curr_data = document.getElementById("journal-entry").value
+        // Now show them we saved and when we did
+        var d = new Date();
+        $('#last-saved').html('Saved! Last: ' + d.toLocaleTimeString());
+        $('#curr-saved').html('Most recent text: ' + curr_data);
+    }
+
+})
